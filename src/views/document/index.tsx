3a76d7fb7930014353664f63cd3aa4b030addbe7
gzdl-strive <228419172@qq.com>
2023-06-06 09:37:56 +0800

    feat: document page basic layout

--- src/views/document/index.tsx
+++ src/views/document/index.tsx
@@ -11,8 +11,20 @@ function Document() {
       <div className={module.description__container}>
         <span className={module.description__text}>{describe}</span>
       </div>
-      <div className={module.wrapper}>
-        <div className={module.sidebar__container}></div>
+      <div className={`${module.wrapper} grid`}>
+        <div className={module.sidebar__container}>
+          <aside className={module.sidebar}>
+            {/* <nav>
+              <ul>
+                <li>Linux常用</li>
+                <li>Vim</li>
+              </ul>
+            </nav> */}
+          </aside>
+          <div className={module.toc__container}>
+            <aside className={module.toc}></aside>
+          </div>
+        </div>
         <main className={module.main__content}>
           <article></article>
         </main>
