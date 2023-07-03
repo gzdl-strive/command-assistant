e329ae799a898f52e9bff6e31c6d1cccc790bdbe
gzdl-strive <228419172@qq.com>
2023-07-03 09:25:06 +0800

    feat: gpt init

--- src/views/gpt/index.tsx
+++ src/views/gpt/index.tsx
@@ -1,9 +1,18 @@
 import module from "./style.module.css";
 
-function GPT() {
+function GptChat() {
   return (
-    <div className={module.container}>{`GPT`}</div>
+    <div className={module.container}>
+      {/* 功能区——侧边栏 */}
+      <aside className={module.function}>
+        aside
+      </aside>
+      {/* 聊天——主内容区 */}
+      <main className={module.chat}>
+        main-gpt-chat
+      </main>
+    </div>
   );
 }
 
-export default GPT;
+export default GptChat;
